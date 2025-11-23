53c358b3ac19aa9c38cd9732e0cc96f767af01ad
istoocold <iksha.hackclub@gmail.com>
2025-11-23 21:29:42 +0530

    2  level complete with art

---
+++ puzzle_container.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var board = $UI/PuzzleContainer/VBoxContainer/PuzzleBoard
+@onready var reset_button = $UI/ButtonContainer/ResetButton
+
+func _ready():
+	
+	reset_button.pressed.connect(_on_reset_pressed)
+	
+	
+	board.initialize_puzzle()
+
+func _on_reset_pressed():
+	board.reset_puzzle()
+	
